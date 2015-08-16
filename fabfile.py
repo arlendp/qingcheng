8d4d1daf75cac653825e1ba88f2ca1fb2bb2801a
Hsiaoming Yang <me@lepture.com>
2015-08-16 23:56:15 +0800

    fabfile uploading to server

--- fabfile.py
+++ fabfile.py
@@ -3,31 +3,34 @@ import os
 import glob
 import hashlib
 import datetime
-from fabric.api import local
+from fabric.api import local, put
 
 
-def upload(filename):
+def _name(filename):
     now = datetime.datetime.now()
 
     with open(filename) as f:
         hsh = hashlib.md5(f.read()).hexdigest()[:6]
 
     name = '%s.%s' % (now.strftime('%Y%m%d'), hsh)
-    name = name + os.path.splitext(filename)[1]
+    return name + os.path.splitext(filename)[1]
+
+
+def upload_qiniu(filename):
+    name = _name(filename)
     local('qboxrsctl put -c python qingcheng/%s %s' % (name, filename))
 
 
+def upload():
+    upload_qiniu('dist/qingcheng.js')
+    upload_qiniu('dist/qingcheng.css')
+
+
 def js():
-    upload('dist/qingcheng.js')
-
-
-def css():
-    upload('dist/qingcheng.css')
-
-
-def assets():
-    js()
-    css()
+    # debug js
+    filename = 'dist/qingcheng.js'
+    name = _name(filename)
+    put(filename, '/var/zerqu/public/assets/%s' % name)
 
 
 def fonts():
